8afdc1fc7e8002528c3e847e86bce69d69e2f8a2
Nebalus <56486387+Nebalus@users.noreply.github.com>
2025-01-20 12:10:01 +0100

    master: DBChange

--- config/mysql/initdb.sql
+++ config/mysql/initdb.sql
@@ -316,7 +316,7 @@ CREATE TABLE `referrals` (
                              `owner_user_id` int UNSIGNED NOT NULL,
                              `code` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
                              `pointer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-                             `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+                             `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
                              `disabled` bit(1) NOT NULL DEFAULT b'0',
                              `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
                              `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
